314d5ee65ff67c1da27b6eba87de73aebbcc098d
kolka72 <kolia199972@gmail.com>
2022-06-26 22:25:13 +0300

    Create game-module

--- src/app.js
+++ src/app.js
@@ -3,14 +3,17 @@ import { ContextMenu } from './menu'
 import { ShapeModule } from './modules/shape.module'
 import { RandomWords } from './modules/text.module'
 import { SoundModule } from './modules/sound.module'
+import { GameModule } from './modules/game.module'
 
 const shapeModule = new ShapeModule('shape', 'Создать фигуру')
 const randomWords = new RandomWords('random', 'Кастомное сообщение')
 const soundModule = new SoundModule('sound', 'Случайный звук')
+const gameModule = new GameModule('game', 'Игра')
 
 const menu = new ContextMenu('#menu', [
   shapeModule,
   randomWords,
-  soundModule
+  soundModule,
+  gameModule
 ])
 menu.render()
